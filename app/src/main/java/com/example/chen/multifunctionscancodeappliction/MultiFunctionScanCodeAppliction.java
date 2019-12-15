28cfeb937945912f3b30807673d6065fc0a0bb7d
HandsomeboyCRJ <2423435674@qq.com>
2019-12-15 21:01:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chen/multifunctionscancodeappliction/MultiFunctionScanCodeAppliction.java
@@ -0,0 +1,13 @@
+package com.example.chen.multifunctionscancodeappliction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiFunctionScanCodeAppliction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_function_scan_code_appliction);
+    }
+}
